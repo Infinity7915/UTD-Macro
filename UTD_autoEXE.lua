37fb7b2092536746c44a9e03b3f48d1e3f4c485a
Infinity🪽 <173856138+Infinity7915@users.noreply.github.com>
2026-06-06 14:34:54 -0500

    Update UTD_autoEXE.lua
    Added instructions for adding the file or LUAU code to a file into the local user's auto-exe folder of their executor.

--- UTD_autoEXE.lua
+++ UTD_autoEXE.lua
@@ -1,3 +1,9 @@
+--[[
+  Instructions:
+    Method_1: Download this file and put the file inside of your executor's auto-exe folder.
+    Method_2: Copy EVEYTHING inside of this file, create a blank txt or lua file inside of your executor's auto-exe folder, then paste EVERYTHING into that blank file and save.
+]]
+
 print("Project Infinity!\n00:00:00 -- Loading Config & Libraries Please wait...")
 task.wait(15)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Infinity7915/UTD-Macro/refs/heads/main/Main.lua"))(true)
