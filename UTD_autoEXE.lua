bf18ca4223dd3e9d89ffe7b39f329d658e539863
Infinity🪽 <173856138+Infinity7915@users.noreply.github.com>
2026-06-06 03:36:08 -0500

    Create UTD_autoEXE.lua

---
+++ UTD_autoEXE.lua
@@ -0,0 +1,4 @@
+print'Project Infinity Injected🪽'
+print(Please wait - Loading Macro...)
+task.wait(15)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Infinity7915/UTD-Macro/refs/heads/main/Main.lua"))(true)
