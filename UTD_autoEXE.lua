7530f4007737399c95922f21876b57328e6343ef
Infinity🪽 <173856138+Infinity7915@users.noreply.github.com>
2026-06-06 14:31:25 -0500

    Update UTD_autoEXE.lua

--- UTD_autoEXE.lua
+++ UTD_autoEXE.lua
@@ -1,3 +1,3 @@
-warn("Project Infinity Injected🪽\n    [+] Loading -> config & Libraries Please wait...")
+print("Project Infinity!\n00:00:00 -- Loading Config & Libraries Please wait...")
 task.wait(15)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Infinity7915/UTD-Macro/refs/heads/main/Main.lua"))(true)
