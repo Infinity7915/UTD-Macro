d26739f0a40080e0302772a7cca2a4fc1091aca0
Infinity🪽 <173856138+Infinity7915@users.noreply.github.com>
2026-06-06 16:12:39 -0500

    Update CatchFish_Event.lua

--- Resources/Events/CatchFish_Event.lua
+++ Resources/Events/CatchFish_Event.lua
@@ -1,2 +1,6 @@
+--[[
+:: You can call me lazy if you wan't, but at least it works.
+]]
+
 local Event = game:GetService("ReplicatedStorage").Modules.GlobalInit.RemoteEvents.PlayerCatchFish
 Event:FireServer();
