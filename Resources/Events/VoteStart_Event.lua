fa9e80b38ae9d9c840eff4b02f3afbba48f6b01f
Infinity🪽 <173856138+Infinity7915@users.noreply.github.com>
2026-06-06 03:30:25 -0500

    Update VoteStart_Event.lua

--- Resources/Events/VoteStart_Event.lua
+++ Resources/Events/VoteStart_Event.lua
@@ -1,1 +1,6 @@
--- Hello World.
+--[[
+:: You can call me lazy if you wan't, but at least it works.
+]]
+
+local Event = game:GetService("ReplicatedStorage").Modules.GlobalInit.RemoteEvents.PlayerVoteToStartMatch
+Event:FireServer()
